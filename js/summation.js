ca4642c54d2c5acaa8dd9681d4dd828405571bbe
Duncan <wangari.dun@gmail.com>
2022-05-10 01:28:26 +0300

    Code wars resumation - summation.js done

---
+++ js/summation.js
@@ -0,0 +1,13 @@
+codeWarsEl.appendChild(document.createElement("li")).classList.add("summation-el")
+
+
+let sum = 0
+let summation = function (num){
+    for(i = 1; i <= num; i++){
+        sum += i
+    }
+    return sum
+}
+
+document.querySelector(".summation-el").textContent = `The sum of 2 is ${summation(2)}`
+//These figure are hard coded
