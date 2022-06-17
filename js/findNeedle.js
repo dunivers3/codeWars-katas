c535c9023a1f530a3617e4ef2573ce47f8b37097
Duncan <wangari.dun@gmail.com>
2022-06-17 08:21:41 +0300

    Adds haystack kata and betterThanAverage kata

---
+++ js/findNeedle.js
@@ -0,0 +1,6 @@
+function findNeedle(haystack) {
+    // your code here
+  console.log(`found the needle at position ${haystack.indexOf("needle")}`)
+}
+
+findNeedle(["dun", "optimus", "mercury", "satan", "empty", "needle", "findThis", "knockout", "lil wayne", "iPhone"])
---
+++ js/iAmBetter.js
@@ -0,0 +1,11 @@
+function iAmBetterThanAverage(classScore, myScore){
+    const averageClassScore = classScore.reduce((a, c) => a + c)/classScore.length
+    if(averageClassScore < myScore){
+        console.log("I am better than average!")
+    }
+    else{
+        console.log("Duncan, you suck!")
+    }
+}
+
+iAmBetterThanAverage([34,55,39,66,23,43,72], 88)
