812b88455071ea59d675eb015a042c5cbcbe1db5
Duncan <wangari.dun@gmail.com>
2022-07-02 20:30:11 +0300

    Adds July 1st kata

---
+++ js/toUpperCase.js
@@ -0,0 +1,12 @@
+function makeUpperCase(str){
+    /**
+     * P: the function takes in a string in any case, always
+     * R: returns the string recieved converted into uppercase
+     * E: "mangoe" => "MANGOE" or "This is a sample COde" => "THIS IS A SAMPLE CODE"
+     * P:
+     * - convert the string recieved to uppercase
+     * - return string
+     */
+    return str.toUpperCase()
+}
+makeUpperCase("The crazy GREY fox jumpED over the lazy brown dog")
