4d3c429d62d6f65950fb8f3d3224de4dcfb65f30
Duncan <wangari.dun@gmail.com>
2022-07-23 20:37:12 +0300

    Adds July 23rd kata

---
+++ js/testing.js
@@ -0,0 +1,16 @@
+const number = function (arr) {
+    /**
+     * P: takes in a list of strings, an array of string, could be empty
+     * R: returns a each string prepended to the correct number, i.e, a numbered string
+     * E: Input --> ["a", "b", "c"] Output --> ["1: a", "2: b", "3: c"]
+     * P:
+     * 1. iterate the array with map method into a new variable
+     * 2. pick the index and the item from the map method
+     * 3. return the index concatinated with the item into the new array
+     */
+    let newArr = arr.map((element, idx) => {
+        return idx.concat(element)
+    })
+    console.log(newArr)
+}
+number(["a", "b", "c"])
